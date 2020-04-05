83735899e2ca05becaab58ea03a7f5d48e9c0db6
phukhieo <akopadung>
2020-04-05 21:25:07 +0700

    Updates

---
+++ precache-manifest.801eb20354846f011d63fed3923626d9.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a129e93b949def01e34fea39d53b7f9",
+    "url": "/makecover/index.html"
+  },
+  {
+    "revision": "b92e70723376c45b7843",
+    "url": "/makecover/static/css/main.4349bbfe.chunk.css"
+  },
+  {
+    "revision": "97d91859a2a9f32ebe0d",
+    "url": "/makecover/static/js/2.75e7c7f4.chunk.js"
+  },
+  {
+    "revision": "91dbce2febfc227edf628f0fd0b15354",
+    "url": "/makecover/static/js/2.75e7c7f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b92e70723376c45b7843",
+    "url": "/makecover/static/js/main.ba190718.chunk.js"
+  },
+  {
+    "revision": "5bf1f472de1c580330bd",
+    "url": "/makecover/static/js/runtime-main.3e8768e1.js"
+  },
+  {
+    "revision": "24cced321296e413845fd3850e71b6a0",
+    "url": "/makecover/static/media/cover01.24cced32.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/makecover/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "da4c82e10f506ceff5b6750d26b9a4cb",
+    "url": "/makecover/static/media/profile2.da4c82e1.jpg"
+  }
+]);
